fa9640a3f0d46cb6e2098e30ec19af9c238b1c74
Bronislav-frontend <marine4bro@gmail.com>
2022-06-05 17:10:50 +0300

    adds scss variables and replace Yup-schema for more readable code

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,43 +1,10 @@
 import { fetchPositions, postUser } from '../../services';
 import SuccessSignup from './SuccessSignup';
+import { SignupSchema } from './Yup.schema';
 import { useEffect, useState } from 'react';
 import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 import s from './PostForm.module.scss';
 import { toast } from 'react-toastify';
-
-const phoneRegExp = /^[+]{1}380([0-9]{9})$/;
-const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg'];
-
-const SignupSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(60, 'Too Long!')
-    .required('Name is required'),
-  phone: Yup.string()
-    .matches(
-      phoneRegExp,
-      'Phone number should be like in this example : +38-XXX-XXX-XX-XX ',
-    )
-    .required('Phone number is required'),
-  email: Yup.string()
-    .email('Invalid email')
-    .max(100, 'Too Long!')
-    .required('Email is required'),
-  position_id: Yup.string().required('Position is required'),
-  photo: Yup.mixed()
-    .required('Photo is required')
-    .test(
-      'fileSize',
-      'File size dhould not exceed 5MB',
-      value => value && value.size <= 5000000,
-    )
-    .test(
-      'fileType',
-      'Photo should be only .jpg or .jpeg formats',
-      value => value && SUPPORTED_FORMATS.includes(value.type),
-    ),
-});
 
 export default function PostForm({
   onSubmitSuccess,
